63a666cadcffd729c266b9a90ca17819793b2127
user <rodionmarkelov01229@gmail.com>
2024-11-02 13:55:58 +0400

    final

--- lab_3/checksum.py
+++ lab_3/checksum.py
@@ -1,10 +1,13 @@
 import re
+import os
 import pandas as pd
 import json
 import hashlib
+
 from typing import List
 
-from paths import PATH_TO_JSON_FILE, PATH_TO_CSV_FILE
+from constants import PATH_TO_JSON_FILE, PATH_TO_CSV_FILE, VARIANT
+from lab_3.constants import VARIANT
 
 """
 В этом модуле обитают функции, необходимые для автоматизированной проверки результатов ваших трудов.
@@ -15,14 +18,6 @@ def calculate_checksum(row_numbers: List[int]) -> str:
     """
     Вычисляет md5 хеш от списка целочисленных значений.
 
-    ВНИМАНИЕ, ВАЖНО! Чтобы сумма получилась корректной, считать, что первая строка с данными csv-файла имеет номер 0
-    Другими словами: В исходном csv 1я строка - заголовки столбцов, 2я и остальные - данные.
-    Соответственно, считаем что у 2 строки файла номер 0, у 3й - номер 1 и так далее.
-
-    Надеюсь, я расписал это максимально подробно.
-    Хотя что-то мне подсказывает, что обязательно найдется человек, у которого с этим возникнут проблемы.
-    Которому я отвечу, что все написано в докстринге ¯\_(ツ)_/¯
-
     :param row_numbers: список целочисленных номеров строк csv-файла, на которых были найдены ошибки валидации
     :return: md5 хеш для проверки через github action
     """
@@ -32,65 +27,82 @@ def calculate_checksum(row_numbers: List[int]) -> str:
 
 def serialize_result(variant: int, checksum: str) -> None:
     """
-    Метод для сериализации результатов лабораторной пишите сами.
-    Вам нужно заполнить данными - номером варианта и контрольной суммой - файл, лежащий в папке с лабораторной.
-    Файл называется, очевидно, result.json.
-
-    ВНИМАНИЕ, ВАЖНО! На json натравлен github action, который проверяет корректность выполнения лабораторной.
-    Так что не перемещайте, не переименовывайте и не изменяйте его структуру, если планируете успешно сдать лабу.
-
+    Метод для сериализации результатов лабораторной.
     :param variant: номер вашего варианта
     :param checksum: контрольная сумма, вычисленная через calculate_checksum()
     """
     try:
-        with open(PATH_TO_JSON_FILE, "r") as f:
+        absolute_path_to_json = os.path.abspath(os.getcwd()) + PATH_TO_JSON_FILE
+        print(absolute_path_to_json)
+        with open(absolute_path_to_json, "r") as f:
             result_data = json.load(f)
 
         result_data["variant"] = variant
         result_data["checksum"] = checksum
 
-        with open(PATH_TO_JSON_FILE, "w") as f:
+        with open(absolute_path_to_json, "w") as f:
             f.write(json.dumps(result_data))
 
     except Exception as e:
-        print(f"Произошла ошибка при обновлении файла: {e}")
+        print(f"Произошла ошибка в функции serialize_result: {e}")
 
 
-def process() -> None:
+def process(csv_file: str) -> List[int]:
+    """
+    Функция обрабатывает значения из csv-файла, а именно ищет несоответствия шаблону,
+    а затем записывает номер строки, в которой присутствует несоответствие.
+    :param csv_file: ссылка на csv-файл
+    :return errors_index: список индексов строк с ошибками
+    """
     parameters = {
         'telephone': r"^\+7-\(\d{3}\)-\d{3}-\d{2}-\d{2}$",
         'height': r"^[1-2]\.\d{2}$",
         'inn': r"^\d{12}$",
         'identifier': r"^\d{2}-\d{2}\/\d{2}$",
         'occupation': r"^[a-zA-Zа-яА-ЯёЁ\s-]+",
-        'latitude': r"^[+-]?(90\.0|0\.0|[0-8]?\d\.\d{6})$",
-        'blood_type': r"^(AB?|B|O)(\+|\-)$",
+        'latitude': r"^[+-]?(90\.0|0\.0|[0-8]?\d\.\d{,6})$",
+        'blood_type': r"^(AB?|B|O)(\+|\u2212)$",
         'issn': r"^\d{4}-\d{4}$",
         'uuid': r"^[a-z0-9]{8}-([a-f0-9]{4}-){3}[a-z0-9]{12}$",
         'date': r"(19|20)\d{2}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])"
     }
 
     try:
-        data = pd.read_csv(PATH_TO_CSV_FILE, encoding='utf-16', sep=';', header=0)
+        data = pd.read_csv(csv_file, encoding='utf-16', sep=';', header=0)
     except Exception as e:
-        print(f"Произошла ошибка при обновлении файла: {e}")
+        print(f"Произошла ошибка в функции process: {e}")
 
     data.columns = ["telephone", "height", "inn", "identifier", "occupation", "latitude", "blood_type", "issn",
                     "uuid", "date"]
 
-    errors = []
+    errors_index = []
 
     for index, row in data.iterrows():
         row_has_error = False
-        for col, pattern in parameters.items():
-            if not re.match(pattern, str(row[col])):
+        for column, pattern in parameters.items():
+            if not re.match(pattern, str(row[column])):
                 row_has_error = True
                 break
         if row_has_error:
-            errors.append(index)
+            errors_index.append(index)
 
-    print(errors)
+    return errors_index
+
+
+def main() -> None:
+    """
+    Эта функция выполняет все действия согласно заданию лабораторой работы,
+    а именно получает список индексов строк с ошибками, подсчитывает контрольную сумму
+    и сериализует результаты в JSON-файл.
+    Ничего не возвращает.
+    :return:
+    """
+    absolute_path_to_csv = os.path.abspath(os.getcwd()) + PATH_TO_CSV_FILE
+    print(absolute_path_to_csv)
+    rows_index = process(absolute_path_to_csv)
+    checksum = calculate_checksum(rows_index)
+    serialize_result(VARIANT, checksum=checksum)
 
 
 if __name__ == "__main__":
-    process()
+    main()
--- lab_3/paths.py
+++
@@ -1,2 +0,0 @@
-PATH_TO_JSON_FILE = 'C:/Users/user/PycharmProjects/prog-instruments-labs/lab_3/result.json'
-PATH_TO_CSV_FILE = 'C:/Users/user/PycharmProjects/prog-instruments-labs/lab_3/16.csv'
